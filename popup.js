117732501b1e20008697d746bf919b6d21cfaa58
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-10-21 15:51:22 -0400

    Adds alarm management screen

--- popup.js
+++ popup.js
@@ -1,1 +1,29 @@
-console.log('Popup is ok!');
+const formatTimeNumber = number => (number > 9 ? `${number}` : `0${number}`);
+
+const setAlarmSelect = () => {
+  const hSelects = document.getElementsByClassName('time__hselect');
+  [...hSelects].forEach(select => {
+    for (let i = 1; i < 25; i++) {
+      const option = document.createElement('option');
+      option.text = formatTimeNumber(i);
+      option.value = formatTimeNumber(i);
+      select.add(option);
+    }
+  });
+  const mSelect = document.getElementsByClassName('time__mselect');
+  [...mSelect].forEach(select => {
+    for (let i = 0; i < 60; i++) {
+      const option = document.createElement('option');
+      option.text = formatTimeNumber(i);
+      option.value = formatTimeNumber(i);
+      select.add(option);
+    }
+  });
+};
+
+const init = () => {
+  setAlarmSelect();
+};
+
+// callbak to start script
+document.addEventListener('DOMContentLoaded', () => init());
